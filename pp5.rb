3be180600c1b82a737caeca86fe2db5280c44c25
Oshi99 <oshiryaeva@protonmail.com>
2018-08-17 19:50:25 -0500

    first commit

---
+++ pp5.rb
@@ -0,0 +1,15 @@
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+sum = 0
+
+munsters.values.each do |item| 
+  sum += item['age'] if item['gender'] == 'male'
+end
+ 
+puts sum
